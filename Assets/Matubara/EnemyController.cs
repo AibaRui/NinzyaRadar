ceb7d01b8b7f3a72af620445d64d0a7313aa09b1
MatubaraTooru <kurakura.ingen@gmail.com>
2022-11-23 00:58:25 +0900

    [fix] Rayを2本目を追加

--- Assets/Matubara/EnemyController.cs
+++ Assets/Matubara/EnemyController.cs
@@ -7,13 +7,14 @@ using UnityEngine.AI;
 public class EnemyController : MonoBehaviour
 {
     [SerializeField, Header("巡回地点のtransformを登録する")] Transform[] _waypoints = default;
-    [SerializeField, Header("Rayの始点")] Transform _rayStartpoint = default;
+    [SerializeField, Header("Rayの始点")] Transform[] _rayStartpoints = default;
     [SerializeField, Header("Rayの長さ")] float _maxDistance;
     [SerializeField, Header("Bulletのプレハブ")] GameObject _bulletPurefab;
     [SerializeField, Header("弾を発射する地点")] Transform _muzzle;
     [SerializeField, Header("弾丸の速度")] float _forcepower;
+    [SerializeField, Header("銃の連射速度")] float _firerate;
     [SerializeField, Header("EnemyのHP")] int _hp = 2;
-    bool _isP = false;
+    float _timer;
     float _saveSpeed;
     NavMeshAgent _navMeshAgent = default;
     int _currentWaypointIndex = 0;
@@ -23,35 +24,42 @@ public class EnemyController : MonoBehaviour
         _navMeshAgent= GetComponent<NavMeshAgent>();
         _navMeshAgent.SetDestination(_waypoints[0].position);
         _saveSpeed = _navMeshAgent.speed;
+        _timer = _firerate;
     }
     void Update()
     {
-        SearchingforPlayer();
+        Ray ray = new Ray(_rayStartpoints[0].position, transform.forward);
+        Ray ray2 = new Ray(_rayStartpoints[1].position, transform.forward);
+
+        _raycastHitPosition = _rayStartpoints[0].position + transform.forward * _maxDistance;
+
+        if (Physics.Raycast(ray, out RaycastHit hit, _maxDistance) && hit.collider.CompareTag("Player") || Physics.Raycast(ray2, out hit, _maxDistance) && hit.collider.CompareTag("Player"))
+        {
+            _raycastHitPosition = hit.point;
+            SearchingforPlayer(true);
+        }
+        else
+        {
+            SearchingforPlayer(false);
+        }
+
+        Debug.DrawLine(_rayStartpoints[0].position, _raycastHitPosition, Color.red);
+        Debug.DrawLine(_rayStartpoints[1].position, _raycastHitPosition, Color.red);
     }
 
-    void SearchingforPlayer()
+    void SearchingforPlayer(bool b)
     {
-        Ray ray = new Ray(_rayStartpoint.position, transform.forward);
-        _raycastHitPosition = _rayStartpoint.position + transform.forward * _maxDistance;
+        MeshRenderer meshRenderer = GetComponent<MeshRenderer>();
 
-        if (Physics.Raycast(ray, out RaycastHit hit, _maxDistance) && hit.collider.CompareTag("Player"))
+        if (b)
         {
-            _isP = true;
-        }
-        else
-        {
-            _isP = false;
-        }
-
-        if (_isP)
-        {
+            meshRenderer.material.color = Color.red;
             _navMeshAgent.speed = 0;
-            _raycastHitPosition = hit.point;
-            Debug.Log("RayがPlayerにヒットしました");
-            Fire();
+            Shoot();
         }
         else
         {
+            meshRenderer.material.color = Color.black;
             _navMeshAgent.speed = _saveSpeed;
             if (_navMeshAgent.remainingDistance <= _navMeshAgent.stoppingDistance)
             {
@@ -59,14 +67,17 @@ public class EnemyController : MonoBehaviour
                 _navMeshAgent.SetDestination(_waypoints[_currentWaypointIndex].position);
             }
         }
-
-        Debug.DrawLine(_rayStartpoint.position, _raycastHitPosition, Color.red);
     }
 
-    void Fire()
+    void Shoot()
     {
-        GameObject go = Instantiate(_bulletPurefab, _muzzle.position, transform.rotation);
-        go.GetComponent<Rigidbody>().AddForce(this.transform.forward * _forcepower, ForceMode.VelocityChange);
+        _timer += Time.deltaTime;
+        if (_timer > _firerate)
+        {
+            GameObject go = Instantiate(_bulletPurefab, _muzzle.position, transform.rotation);
+            go.GetComponent<Rigidbody>().AddForce(this.transform.forward * _forcepower, ForceMode.VelocityChange);
+            _timer = 0;
+        }
     }
     private void OnTriggerEnter(Collider other)
     {
